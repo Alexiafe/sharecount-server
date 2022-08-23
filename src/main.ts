189b1034a3db5b244a0a2d80fbd95a14a966afec
Alexia <alexiaferric@gmail.com>
2022-08-23 20:51:16 +0200

    Add enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import cors from 'cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors()
 
   const config = new DocumentBuilder().setTitle('ShareCount').setDescription('The ShareCount API description').setVersion('1.0').build()
   const document = SwaggerModule.createDocument(app, config)
