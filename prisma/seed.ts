cac52cd22654cccd11bda1751f29a356019d05fc
Alexia Ferric <alexia@ferric.fr>
2023-01-15 20:52:34 +0700

    Seed more data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,12 +5,29 @@ const prisma = new PrismaClient()
 
 const sharecountData: Prisma.SharecountCreateInput[] = [
   {
+    name: 'ShareCount 0',
+    currency: 'EUR',
+    participants: {
+      create: [
+        {
+          name: 'Alexia',
+        },
+        {
+          name: 'Louis',
+        },
+      ],
+    },
+  },
+  {
     name: 'ShareCount 1',
     currency: 'EUR',
     participants: {
       create: [
         {
           name: 'Alexia',
+        },
+        {
+          name: 'Louis',
         },
       ],
     },
@@ -23,12 +40,21 @@ const userData: Prisma.UserCreateInput[] = [
     userInSharecount: {
       create: [
         {
-          participant: {
-            connect: { id: 1 },
-          },
           sharecount: {
             connect: { id: 1 },
+          },
+          participant: {
+            connect: { id: 1 },
           }
+        },
+        {
+          sharecount: {
+            connect: { id: 2 },
+          },
+          participant: {
+            connect: { id: 1 },
+          }
+
         }
       ],
     }
@@ -48,13 +74,13 @@ async function main() {
     const user = await prisma.user.create({
       data: u,
     })
-    console.log(`Created user with id: ${user.email}`)
+    console.log(`Created user with email: ${user.email}`)
   }
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 20; i++) {
     const expense = await prisma.expense.create({
       data: {
         name: 'Expense ' + i,
-        amount_total: 10,
+        amount_total: 20,
         date: moment().utcOffset(0).set({ hour: 0, minute: 0, second: 0, millisecond: 0 }).format(),
         sharecount: {
           connect: { id: 1 },
@@ -69,11 +95,72 @@ async function main() {
               participant: {
                 connect: { id: 1 },
               }
+            },
+            {
+              amount: 10,
+              participant: {
+                connect: { id: 2 },
+              }
             }]
         },
       },
     })
     console.log(`Created expense with id: ${expense.id}`)
+  }
+  for (let i = 0; i < 3; i++) {
+    const expense = await prisma.expense.create({
+      data: {
+        name: 'Expense ' + i,
+        amount_total: 20,
+        date: moment().utcOffset(0).set({ hour: 0, minute: 0, second: 0, millisecond: 0 }).format(),
+        sharecount: {
+          connect: { id: 2 },
+        },
+        owner: {
+          connect: { id: 1 },
+        },
+        partakers: {
+          create: [
+            {
+              amount: 10,
+              participant: {
+                connect: { id: 1 },
+              }
+            },
+            {
+              amount: 10,
+              participant: {
+                connect: { id: 2 },
+              }
+            }]
+        },
+      },
+    })
+    console.log(`Created expense with id: ${expense.id}`)
+  }
+  for (let i = 2; i < 20; i++) {
+    const sharecount = await prisma.sharecount.create({
+      data: {
+        name: 'Sharecount ' + i,
+        currency: 'EUR',
+        userInSharecount: {
+          create: [
+            {
+              participant: { connect: { id: 1 } },
+              user: { connect: { email: 'alexiaferric@gmail.com' } }
+            }
+          ]
+        },
+        participants: {
+          create: [
+            {
+              name: 'Alexia',
+            },
+          ],
+        },
+      }
+    })
+    console.log(`Created sharecount with id: ${sharecount.id}`)
   }
   console.log(`Seeding finished.`)
 }
